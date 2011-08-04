2f3ac911a1c7a5bf58a784a290f73cf8e4c4e903
Carlos Camargo <cicamargoba@gmail.com>
2011-08-03 19:15:49 -0500

    Adición de archivos para probar una SIE nueva

--- binaries/reflash/program_rootfs.sh
+++ binaries/reflash/program_rootfs.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-ROOTFS=openwrt-xburst-qi_lb60-root.ubi
+ROOTFS=openwrt-xburst-rootfs.ubi
 sudo usbboot -f ./usbboot_2gb_nand.cfg  -c "boot"
-sudo usbboot -f ./usbboot_2gb_nand.cfg  -c "nerase 16 512 0 0"
 sudo usbboot -f ./usbboot_2gb_nand.cfg  -c "nprog 2048 $ROOTFS 0 0 -n"
