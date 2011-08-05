5588b50968fd508d73068a0c286c39ddabcc7585
Carlos Camargo <cicamargoba@gmail.com>
2011-08-05 11:17:50 -0500

    Cambio en la capa de silk screen dela tarjeta SIE en la versión de Orcad

--- reflash/program_rootfs.sh
+++ reflash/program_rootfs.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+#ROOTFS=openwrt-xburst-sie-rootfs.ubi
 ROOTFS=openwrt-xburst-rootfs.ubi
 sudo usbboot -f ./usbboot_2gb_nand.cfg  -c "boot"
 sudo usbboot -f ./usbboot_2gb_nand.cfg  -c "nprog 2048 $ROOTFS 0 0 -n"
