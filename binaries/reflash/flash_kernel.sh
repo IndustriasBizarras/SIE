ccf27935c828a68357f3fd7144b6e9008bcfad64
Carlos Camargo <cicamargoba@gmail.com>
2011-08-05 08:42:39 -0500

    Adición del sistema de archivos para pruebas basado en la distribución oficial de openwrt

--- binaries/reflash/flash_kernel.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-sudo usbboot -f ./usbboot_2gb_nand.cfg  -c "boot"
-sudo usbboot -f ./usbboot_2gb_nand.cfg  -c "nprog 1024 openwrt-xburst-uImage.bin 0 0 -n"
---
+++ reflash/program_rootfs.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+ROOTFS=openwrt-xburst-rootfs.ubi
+sudo usbboot -f ./usbboot_2gb_nand.cfg  -c "boot"
+sudo usbboot -f ./usbboot_2gb_nand.cfg  -c "nprog 2048 $ROOTFS 0 0 -n"
